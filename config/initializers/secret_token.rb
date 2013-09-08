601db75f5ecfb62a45de6c2f40668c41db31eeba
Matt Hess <matthew.a.hess@gmail.com>
2013-09-08 15:35:05 -0700

    New rails 4.0.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DayPlanner::Application.config.secret_key_base = '40f228acd8bd6ff56e14788e0d621a011004869c269d671d5970618dcefa02a8664c26a165f0ec9572c6e100f5253ddd702a12a0b9d5954ebde5c44a11a3da5f'
