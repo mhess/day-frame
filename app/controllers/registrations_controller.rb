afc0aebf26fa877e2903dae3f8411dd204753985
Matt Hess <matthew.a.hess@gmail.com>
2013-10-27 21:54:35 -0700

    Fix devise parameter bug with sign up.
    * Modify devise registrations parameter sanitization to allow "name" field to be
      submitted.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   
-  def account_update_params
-    permitted = resource_class.authentication_keys + [:password, :password_confirmation,
-                                                      :current_password, :wake, :sleep, :name]
-    params.require(:user).permit(*permitted)
-  end
-  
-  def update_resource(resource, strong_params)
-    if (strong_params[:password] + strong_params[:password_confirmation]).empty? then
-      [:password, :password_confirmation].each {|k| strong_params.delete(k)}
-    end
-    resource.update(strong_params)
+  protected
+
+  def devise_parameter_sanitizer
+    User::ParameterSanitizer.new(User, :user, params)
   end
 end
---
+++ lib/user_sanitizer.rb
@@ -0,0 +1,14 @@
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+
+  private
+
+  def sign_up
+    default_params.permit(:name, :email, :password,
+                          :password_confirmation)
+  end
+
+  def account_update
+    default_params.permit(:name, :email, :password,
+                          :password_confirmation, :current_password)
+  end
+end
