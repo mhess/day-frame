2d73148f65c4e11ccbbc185c4bda91bafa7d44a8
Matt Hess <matthew.a.hess@gmail.com>
2013-10-27 22:28:43 -0700

    Switch to postgres.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,9 @@ gem 'angular-ui-bootstrap-rails'
 gem 'jquery-ui-rails'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
+
+gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20130924180805) do
     t.datetime "updated_at"
   end
 
-  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20130924180805) do
     t.integer  "sleep",                  default: 1100
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
