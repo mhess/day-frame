65af341ecf9c49292284d25530ce0117a52ac544
Matt Hess <matthew.a.hess@gmail.com>
2013-12-26 00:59:07 -0800

    Change compass-rails dependency version.  Fix prod config typo.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'jquery-ui-rails'
 gem 'jbuilder', '~> 1.2'
 gem 'sass-rails', '~> 4.0.0'
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass'
-gem 'compass-rails', "~> 2.0.alpha.0"
+gem 'compass-rails', "~> 1.1.2"
 
 group :production do
   gem 'uglifier', '>= 1.3.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ DayFrame::Application.configure do
     port: 587,
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD']
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
